3a5eb13d53cf3dfb232a3daba2ec38b4abe6aef4
An-jihyun <anjh322@gmail.com>
2025-07-10 20:24:35 +0900

    feat: 레시피 CRUD 기능 구현

--- src/main/java/com/example/recipeapp/domain/like/domain/model/entity/Likes.java
+++ src/main/java/com/example/recipeapp/domain/like/domain/model/entity/Likes.java
@@ -1,24 +1,24 @@
-package com.example.recipeapp.domain.like.domain.model.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "likes")
-public class Likes {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user; // 좋아요 누른 사람
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Recipe recipe; // 좋아요 대상 게시글
-
-    private LocalDateTime createdAt;
-
-}
+//package com.example.recipeapp.domain.like.domain.model.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "likes")
+//public class Likes {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User user; // 좋아요 누른 사람
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Recipe recipe; // 좋아요 대상 게시글
+//
+//    private LocalDateTime createdAt;
+//
+//}
