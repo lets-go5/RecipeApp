48004888b75d923e45de08739f02d70714cfaf99
handokei <ehgks994@naver.com>
2025-07-14 16:11:05 +0900

    Update RecipeRepositoryCustom.java

--- src/main/java/com/example/recipeapp/domain/recipes/domain/repository/RecipeRepositoryCustom.java
+++ src/main/java/com/example/recipeapp/domain/recipes/domain/repository/RecipeRepositoryCustom.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface RecipeRepositoryCustom {
+public interface RecipeRepositorySearchCustom {
     Page<RecipeResponse> search(String keyword, Pageable pageable);
 
     List<Recipe> searchTopNByKeyword(String keyword, int limit);
