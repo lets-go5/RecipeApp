b6798be7ed828063d8a68092b12babf4f5d724b7
handokei <ehgks994@naver.com>
2025-07-14 21:35:53 +0900

    refactor: 응답 API 시간 -> @JsonFormat 을 통한 날짜 포맷 추가,  JacksonConfig 응답용 @Primary 로 추가 구현 리펙토링

--- src/main/java/com/example/recipeapp/global/config/JacksonConfig.java
+++ src/main/java/com/example/recipeapp/global/config/JacksonConfig.java
@@ -2,19 +2,32 @@ package com.example.recipeapp.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.activateDefaultTyping( // 직렬화 시 타입 정보 같이 담기
-                BasicPolymorphicTypeValidator.builder().allowIfSubType(Object.class).build(),
-                ObjectMapper.DefaultTyping.EVERYTHING   // 모든 타입 허용
+        objectMapper.registerModule(new JavaTimeModule());
+        // 타입 정보 안붙임
+        return objectMapper;
+    }
+
+    // 캐시 저장/복원용 (타입 정보 포함)
+    @Bean(name = "cacheObjectMapper")
+    public ObjectMapper cacheObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.activateDefaultTyping(  // 데이터 타입 깉이 가져오기
+                BasicPolymorphicTypeValidator.builder().allowIfSubType(Object.class).build(), // 모든데이터 타입 허용
+                ObjectMapper.DefaultTyping.EVERYTHING // 객체에 타입 정보 붙이기
         );
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
 }
